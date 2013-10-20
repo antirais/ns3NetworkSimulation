f6d9a292b3f473c833295adaf1ae65fd710a3bac
antirais <antirais@gmail.com>
2013-10-20 22:40:53 +0300

    improved analyze script

--- analyze.sh
+++ analyze.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-#find $out_dir/*.pcap -type f 
+#find $out_dir/*.pcap -type f
 ro_IP="192.168.7.1.9"
 lo_IP="192.168.107.2"
 out_dir="./output"
 tcpdump=`which tcpdump`
-ro_server=`find ./output/ro-csma* -type f | sed -n 1p`
-#ro_router="ro-csma-10-1.pcap"
-lo_server=`find ./output/lo-csma* -type f | sed -n 2p`
-#lo_router="lo-csma-22-1.pcap"
-ns3_args=$1
 
 find_server=``
 
 rm -f $out_dir/*.pcap
-python scratch/main.py $ns3_args
+python scratch/main.py --cloudNodes=$1
+ro_server=`find ./output/ro-csma* -type f | sort -V | sed -n 1p`
+ro_wifi_gw=`find ./output/ro-wifi* -type f | sort -V | sed -n 1p`
+
+lo_server=`find ./output/lo-csma* -type f | sort -V | sed -n 2p`
+lo_lan_gw=`find ./output/lo-csma* -type f | sort -V | sed -n 1p`
+lo_wifi_gw=`find ./output/lo-wifi* -type f | sort -V | sed -n 1p`
 
 echo "right office server responses:"
 $tcpdump -n -tt -r $ro_server | grep "$ro_IP >" | wc -l
+echo "right office wifi gateway traffic:"
+$tcpdump -n -tt -r $ro_wifi_gw | grep "$ro_IP >" | wc -l
+
 echo "left office server responses:"
 $tcpdump -n -tt -r $lo_server | grep "$lo_IP >" | wc -l
-#tcpdump -n -tt -r output/lo-csma-21-1.pcap | grep "192.168.107.2 >" | wc -l
+echo "left office LAN gateway traffic:"
+$tcpdump -n -tt -r $lo_lan_gw | grep "$lo_IP >" | wc -l
+echo "left office wifi gateway traffic:"
+$tcpdump -n -tt -r $lo_wifi_gw | grep "$lo_IP >" | wc -l
